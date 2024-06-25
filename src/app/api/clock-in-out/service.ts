c0869295287793a353f3b66ae4aac6c4db73c182
Yossapol Witayanont <reef_3llen@hotmail.com>
2024-06-25 08:41:51 +0700

    feat: :rocket: clock-in-out api

---
+++ src/app/api/clock-in-out/service.ts
@@ -0,0 +1,130 @@
+import dayjs from "@/lib/dayjs";
+import clientPromise from "@/lib/mongo";
+import { ObjectId } from "mongodb";
+
+const DB_COLLECTION = 'clock_in_out';
+async function getCollection() {
+  const client = await clientPromise;
+  const db = client.db();
+
+  return db.collection(DB_COLLECTION);
+}
+
+function getTodayRange() {
+  const now = dayjs().tz();
+  const startOfDay = now.startOf('day').toDate();
+  const endOfDay = now.endOf('day').toDate();
+
+  return { now: now.toDate(), startOfDay, endOfDay };
+}
+
+async function findTodayRecords(
+  employeeId: string,
+  startOfDay: Date,
+  endOfDay: Date
+) {
+  const collection = await getCollection();
+
+  return collection.find({
+    employeeId,
+    timestamp: {
+      $gte: startOfDay,
+      $lte: endOfDay
+    }
+  }).toArray();
+}
+
+function toInsertData(
+  employeeId: string,
+  employee: object,
+  timestamp: Date,
+  type: string,
+) {
+  return {
+    employeeId,
+    employee,
+    timestamp,
+    date: dayjs(timestamp).tz().format('YYYY-MM-DD'),
+    time: dayjs(timestamp).tz().format('HH:mm:ss'),
+    type
+  };
+}
+
+async function insertCheckIn(
+  employeeId: string,
+  employee: object,
+  timestamp: Date
+) {
+  const collection = await getCollection();
+  const data = toInsertData(employeeId, employee, timestamp, 'IN');
+
+  return collection.insertOne(data);
+}
+
+async function insertCheckOut(
+  employeeId: string,
+  employee: object,
+  timestamp: Date
+) {
+  const collection = await getCollection();
+  const data = toInsertData(employeeId, employee, timestamp, 'OUT');
+
+  return collection.insertOne(data);
+}
+
+async function updateCheckOut(
+  _id: ObjectId,
+  timestamp: Date
+) {
+  const collection = await getCollection();
+
+  return collection.updateOne(
+    { _id },
+    {
+      $set: {
+        timestamp,
+        type: 'OUT'
+      }
+    }
+  );
+
+}
+
+export async function clockInOut(
+  employeeId: string,
+  employee: object,
+) {
+  const { now, startOfDay, endOfDay } = getTodayRange();
+  const todayRecords = await findTodayRecords(
+    employeeId,
+    startOfDay,
+    endOfDay
+  );
+
+  let message = '';
+  if (todayRecords.length === 0) {
+    await insertCheckIn(employeeId, employee, now);
+    message = 'CHECKED_IN';
+  }
+  if (todayRecords.length === 1) {
+    await insertCheckOut(employeeId, employee, now);
+    message = 'CHECKED_OUT';
+  }
+  if (todayRecords.length > 1) {
+    const _id = todayRecords[todayRecords.length - 1]._id;
+    await updateCheckOut(_id, now)
+    message = 'CHECKED_OUT';
+  }
+
+  return { message };
+}
+
+export async function getLatestActivities() {
+  const collection = await getCollection();
+
+  return collection
+    .find({})
+    .sort({ timestamp: -1 })
+    .limit(10)
+    .toArray();
+}
